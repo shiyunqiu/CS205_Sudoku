bd6f70d43fea85a6359506ea0654016d36a42f8f
Yiqi Xie <yiqi_xie@g.harvard.edu>
2018-04-19 15:00:52 -0400

    fixed the omp timer issue

--- code/main.cpp
+++ code/main.cpp
@@ -10,6 +10,9 @@
 #include <ctime>
 // #include "Sudoku_serial.hpp"
 #include "Sudoku_parallel.hpp"
+#include "omp.h"
+
+inline double wtime() {return omp_get_wtime();}
 
 int main(int argc, const char * argv[]) {
     // int len = 16;
@@ -21,12 +24,17 @@ int main(int argc, const char * argv[]) {
     // double elapsed_secs = double(end - begin) / CLOCKS_PER_SEC;
     // std::cout << elapsed_secs << " seconds.\n";
     int len = 16;
+    omp_set_num_threads(2);
     std::string filename ="/Users/shell/Documents/Projects/cs205_final/test_cases/test16_2.sdk";
     clock_t begin = clock();
+    double wbegin = wtime();
     SudokuParallel sp(filename, len);
     sp.solve();
+    double wend = wtime();
     clock_t end = clock();
     double elapsed_secs = double(end - begin) / CLOCKS_PER_SEC;
-    std::cout << elapsed_secs << " seconds.\n";
+    double welapsed_secs = wend - wbegin;
+    std::cout << elapsed_secs << " seconds, by clock.\n";
+    std::cout << welapsed_secs << " seconds, by omp wtime.\n";
     return 0;
 }
